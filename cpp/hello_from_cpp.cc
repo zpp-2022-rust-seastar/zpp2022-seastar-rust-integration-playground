b6fde4a22ac9df813b9933d6f93cf5959fae7de0
kfernandez31 <kacper.fernandez@gmail.com>
2022-11-17 20:17:34 +0100

    Added some arguments for the "hello" calls

--- cpp/hello_from_cpp.cc
+++ cpp/hello_from_cpp.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "hello_from_cpp.h"
 
-extern "C" void hello_from_cpp() {
-    std::cout << "Hello from C++!\n";
+extern "C" void hello_from_cpp(int x) {
+    std::cout << "Hello from C++ with value " << x << "!\n";
 }
