b7cb989865294ab271bd660eec92fde4b7c4480b
Patryk Jędrzejczak <patryk.jed123@gmail.com>
2022-11-19 19:35:25 +0100

    Add STORE/LOAD calls (currently only printing) from Rust

--- server/rust_ffi/rust_ffi.h
+++ server/rust_ffi/rust_ffi.h
@@ -1,1 +1,5 @@
-extern "C" void hello_from_cpp(int x);
+extern "C" void store_from_rust(const char *key, const char *value);
+
+extern "C" char * load_from_rust(const char *key);
+
+extern "C" void free_from_rust(char *ptr);
