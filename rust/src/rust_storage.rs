6c8dfa7bd05dd5def9b4e231cd10ffa7f393dfc2
Grzegorz Nowakowski <kamikazelol@op.pl>
2022-12-15 20:28:24 +0100

    Added static references

--- rust/src/rust_storage.rs
+++ rust/src/rust_storage.rs
@@ -7,15 +7,15 @@ pub struct RustStorage {
 }
 
 impl RustStorage {
-    pub fn store(&mut self, key: &str, val: &str) -> StoreFuture {
+    pub fn store(&'static mut self, key: &'static str, val: &'static str) -> StoreFuture {
         StoreFuture::infallible(async {
             seastar_sleep_1s().await;
             self.dict.insert(key.to_string(), val.to_string());
         })
     }
 
-    pub fn load(&self, key: &str) -> LoadFuture {
-        LoadFuture::fallible(async {
+    pub fn load(&'static self, key: &'static str) -> LoadFuture {
+        LoadFuture::fallible(async move {
             seastar_sleep_1s().await;
             match self.dict.get(key) {
                 None => Err(CxxAsyncException::new(format!("entry for {key} not found").to_owned().into_boxed_str())),
