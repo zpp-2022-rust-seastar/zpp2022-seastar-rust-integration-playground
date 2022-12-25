363152175ccb7edc27bc0f692267abacfebab4f8
kfernandez31 <kacper.fernandez@gmail.com>
2022-12-25 19:50:27 +0100

    Fixed build.rs to actually copy cxx-async's headers. Still not visible from server/ffi.hh though

--- rust/build.rs
+++ rust/build.rs
@@ -1,22 +1,27 @@
 use std::{fs, env};
 use std::path::Path;
+use std::io::Write;
 
 fn copy_cxx_async_headers() {
-    let common_path_prefix = env::current_dir().unwrap().parent().unwrap().join("build").join("rust");
-    let dest_include_path = common_path_prefix.join("cxxbridge").join("rust");
-    let cxx_async_path = common_path_prefix.join("debug").join("build");
+    let build_path = env::current_dir().unwrap().parent().unwrap().join("build").join("rust");
+    let dest_include_path = build_path.join("cxxbridge").join("rust");
+    let deps_path = build_path.join("debug").join("build");
 
     let include_path = Path::new("out").join("include").join("rust");
-    let src_include_dir = fs::read_dir(cxx_async_path)
+    let cxx_async_dir = 
+        fs::read_dir(deps_path)
         .unwrap()
         .flatten()
         .filter(|dir| dir.file_name().to_str().unwrap().starts_with("cxx-async"))
         .filter(|dir| dir.path().join(&include_path).exists())
         .next()
         .unwrap();
+    let cxx_async_include_dir = 
+        fs::read_dir(cxx_async_dir.path().join(include_path))
+        .unwrap();
 
-    for header in fs::read_dir(src_include_dir.path()).unwrap().flatten() {
-        drop(fs::copy(header.path(), &dest_include_path));
+    for header in cxx_async_include_dir.flatten() {
+        drop(fs::copy(header.path(), &dest_include_path.join(header.file_name())));
     }
 }
 
